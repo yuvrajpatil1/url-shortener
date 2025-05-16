0865d86e5a595e8c00348cabacda052565cab2c4
Yuvraj Patil <yuvrajpatil12092004@gmail.com>
2025-05-16 13:32:38 +0530

    footer-added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,8 @@
 function Footer() {
   return (
-    <div className="text-sm text-white p-4 border-t border-[#3a3a3a] text-center bg-black">
+    <div className="text-sm text-[white] p-4 border-t-2 border-[#E5EFEB] text-center">
       Developed with ❤️ by{" "}
-      <a
-        className="underline"
-        href="https://www.linkedin.com/in/yuvrajkpatil"
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a className="underline" href="https://www.linkedin.com/in/yuvrajkpatil">
         Yuvraj Patil
       </a>
       .

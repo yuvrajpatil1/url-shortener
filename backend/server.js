a2aa485323bb55e9c936efd371b9ed8fc716bf53
Yuvraj Patil <yuvrajpatil12092004@gmail.com>
2025-05-16 14:15:02 +0530

    cors-origin

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://slashbyhash.vercel.app", // Replace with your frontend's domain
+    origin: "*", // Replace with your frontend's domain
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,

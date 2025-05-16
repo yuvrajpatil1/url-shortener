f75fe8b08a18ce36ba035d7a20a69b9fe2b4d716
Yuvraj Patil <yuvrajpatil12092004@gmail.com>
2025-05-16 15:13:09 +0530

    cors-conflict

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,9 @@ const jwt = require("jsonwebtoken");
 const User = require("./models/User");
 
 const app = express();
-
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
+
+app.use(cors());
 
 require("dotenv").config();
 
